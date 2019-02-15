a0e779ff9dd24bdf91268765af54c139cec4dcde
Priyanka Sabhagani <priyanka.sabhagani@bookmyshow.com>
2019-02-15 14:57:39 +0530

    make it a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,52 +5,74 @@ void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final title = 'Grid List';
-
-    RandomColor _randomColor = RandomColor();
-
-    Color _color = _randomColor.randomColor(
-      // colorBrightness: ColorBrightness.light
-    );
-
     return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
+      title: 'Grid list Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Grid'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
         body: GridView.count(
           // Create a grid with 2 columns. If you change the scrollDirection to
           // horizontal, this would produce 2 rows.
           crossAxisCount: 2,
           // Generate 100 Widgets that display their index in the List
           children: List.generate(100, (index) {
-            return Center(
+            Color _color = RandomColor().randomColor(colorSaturation: ColorSaturation.lowSaturation);
+            return InkWell(
                 child: Container(
-                // color:  _randomColor.randomColor(colorSaturation: ColorSaturation.lowSaturation),
-                // padding: const EdgeInsets.all(50.0),
-                decoration: new BoxDecoration(
-                  color: _randomColor.randomColor(colorSaturation: ColorSaturation.lowSaturation),
-                ),
-                constraints: new BoxConstraints.expand(
-                  // width: 250.0,
-                ),
-                alignment: FractionalOffset.center,
-                child: Text(
-                  'Item $index',
-                  style: Theme.of(context).textTheme.headline,
-                ),
-                // width: 220,
-                // height: 150,
+                  // color:  _randomColor.randomColor(colorSaturation: ColorSaturation.lowSaturation),
+                  // margin: const EdgeInsets.fromLTRB(16,8,8,16),
+                  decoration: new BoxDecoration(
+                    color: _color,
+                  ),
+                  constraints: new BoxConstraints.expand(
+                    // width: 250.0,
+                  ),
+                  alignment: FractionalOffset.center,
+                  child: Text(
+                    'Item $index',
+                    style: Theme.of(context).textTheme.headline,
+                  ),
               ),
-
+              onTap: () {
+                      print("tapped on container");
+                      },
             );
           }),
         ),
-      ),
     );
   }
 }
